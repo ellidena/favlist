0275e2dc6e61ba8af64f52bb16e6f98544c7f3b2
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-29 00:03:53 +0100

    first commit

---
+++ src/main/java/com/favlist/FavlistApplication.java
@@ -0,0 +1,13 @@
+package com.favlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/favlist/FavlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
