30ce1c6de0afc9502054a39059432407c1034cf7
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 14:58:25 +0200

    add UserRepository annotation and inject JdbcTemplate

--- src/main/java/com/favlist/repository/UserRepository.java
+++ src/main/java/com/favlist/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.favlist.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
+    private final JdbcTemplate jdbc;
+
+    public UserRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
 }
