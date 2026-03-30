a164ad10467f99c91757fc921feb9475824d5f77
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 15:05:07 +0200

    Add ItemService annotation and constructor injection

--- src/main/java/com/favlist/service/ItemService.java
+++ src/main/java/com/favlist/service/ItemService.java
@@ -1,4 +1,26 @@
 package com.favlist.service;
 
+import com.favlist.repository.CategoryRepository;
+import com.favlist.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
+    /*
+    listing items
+    filtering by category
+    showing item details
+    uses:
+    ItemRepo, CategoryRepo
+     */
+    private final ItemRepository itemRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ItemService(
+            ItemRepository itemRepository,
+            CategoryRepository categoryRepository
+    ) {
+        this.itemRepository = itemRepository;
+        this.categoryRepository = categoryRepository;
+    }
 }
