d02ba0a09612a8584501d830b09dae07068e7544
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 17:18:19 +0200

    Add rowMapper for Category

--- src/main/java/com/favlist/repository/CategoryRepository.java
+++ src/main/java/com/favlist/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.favlist.repository;
 
 import com.favlist.model.Category;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +12,11 @@ public class CategoryRepository {
     public CategoryRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
+
+    private final RowMapper<Category> categoryRowMapper = (rs, rowNum) -> {
+        Category c = new Category();
+        c.setCategoryId(rs.getInt("category_id"));
+        c.setName(rs.getString("name"));
+        return c;
+    };
 }
