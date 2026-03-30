e1dc20ec606bcc23674beea2511c610312e3eb76
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 20:47:05 +0200

    Add CategoryRepositoryTest class

---
+++ src/test/java/com/favlist/wishlist_h2/repository/CategoryRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.favlist.wishlist_h2.repository;
+
+import com.favlist.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:h2init.sql", executionPhase = BEFORE_TEST_METHOD)
+public class CategoryRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+}
