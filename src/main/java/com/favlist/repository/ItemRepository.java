6b3f62754c78f997cba9ed3ff511cfa6fdd5759e
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 21:56:22 +0200

    Add itemRowMapper

--- src/main/java/com/favlist/repository/ItemRepository.java
+++ src/main/java/com/favlist/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.favlist.repository;
 
+import com.favlist.model.Item;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +12,13 @@ public class ItemRepository {
     public ItemRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
+
+    private final RowMapper<Item> itemRowMapper = (rs, rowNum) -> {
+        Item i = new Item();
+        i.setItemId(rs.getInt("item_id"));
+        i.setName(rs.getString("name"));
+        i.setDescription(rs.getString("description"));
+        i.setCategoryId(rs.getInt("category_id"));
+        return i;
+    };
 }
