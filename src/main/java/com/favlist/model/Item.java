6f64e7c120788011450faa3b24e53d34bbac6a0b
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 13:40:43 +0200

    Add fields, constructor, and accessors to Item model

--- src/main/java/com/favlist/model/Item.java
+++ src/main/java/com/favlist/model/Item.java
@@ -1,4 +1,33 @@
 package com.favlist.model;
 
 public class Item {
+    private int id;
+    private String name;
+    private String description;
+
+    public Item(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
