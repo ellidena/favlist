aefb01718758042277456d45ab57738992caf382
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-30 13:46:33 +0200

    Add fields, constructor, accessors to WishlistEntry model

--- src/main/java/com/favlist/model/WishlistEntry.java
+++ src/main/java/com/favlist/model/WishlistEntry.java
@@ -1,4 +1,42 @@
 package com.favlist.model;
 
 public class WishlistEntry {
+    private int id;
+    private int itemId;
+    private int wishlistId;
+    private String note;
+
+    public WishlistEntry(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
