aae5bb241506feb332252c43fe171859ad00bbd1
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-31 22:41:09 +0200

    Add WishlistEntry RowMapper

--- src/main/java/com/favlist/repository/WishlistEntryRepository.java
+++ src/main/java/com/favlist/repository/WishlistEntryRepository.java
@@ -1,6 +1,8 @@
 package com.favlist.repository;
 
+import com.favlist.model.WishlistEntry;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +12,13 @@ public class WishlistEntryRepository {
     public WishlistEntryRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
+
+    private final RowMapper<WishlistEntry> wishlistEntryRowMapper = (rs, rowNum) -> {
+        WishlistEntry e = new WishlistEntry();
+        e.setWishlistId(rs.getInt("wishlist_id"));
+        e.setItemId(rs.getInt("item_id"));
+        e.setNote(rs.getString("note"));
+        return e;
+    };
+
 }
