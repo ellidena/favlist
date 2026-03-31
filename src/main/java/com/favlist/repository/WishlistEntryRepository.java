5dffe89bda6c4018f54c8dfdb00b39094fbd6400
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-31 23:06:37 +0200

    Add findByWishlistId and test class with inital test method

--- src/main/java/com/favlist/repository/WishlistEntryRepository.java
+++ src/main/java/com/favlist/repository/WishlistEntryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class WishlistEntryRepository {
     private final JdbcTemplate jdbc;
@@ -20,5 +22,11 @@ public class WishlistEntryRepository {
         e.setNote(rs.getString("note"));
         return e;
     };
+
+    // return all items in a wishlist
+    public List<WishlistEntry> findByWishlistId(int wishlistId) {
+        String sql = "SELECT * FROM wishlist_entry WHERE wishlist_id = ?";
+        return jdbc.query(sql, wishlistEntryRowMapper, wishlistId);
+    }
 
 }
---
+++ src/test/java/com/favlist/wishlist_h2/repository/WishlistEntryRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.favlist.wishlist_h2.repository;
+
+import com.favlist.model.WishlistEntry;
+import com.favlist.repository.WishlistEntryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:h2init.sql", executionPhase = BEFORE_TEST_METHOD)
+public class WishlistEntryRepositoryTest {
+
+    @Autowired
+    private WishlistEntryRepository wishlistEntryRepository;
+
+    @Test
+    void findByWishlistId_returnsAllEntries() {
+        List<WishlistEntry> entries = wishlistEntryRepository.findByWishlistId(1);
+
+        assertThat(entries).hasSize(2);
+    }
+}
