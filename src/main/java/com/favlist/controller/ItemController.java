3eefa69c8b415a0a3a399457e6d06f28c13027ef
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-03 20:19:11 +0200

    Add itemDetails endpoint

--- src/main/java/com/favlist/controller/ItemController.java
+++ src/main/java/com/favlist/controller/ItemController.java
@@ -1,10 +1,12 @@
 package com.favlist.controller;
 
+import com.favlist.model.Category;
 import com.favlist.model.Item;
 import com.favlist.service.ItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,10 +29,16 @@ public class ItemController {
         return "items/list";
     }
 
+    @GetMapping("/{id}")
+    public String itemDetails(@PathVariable int id, Model model) {
+        model.addAttribute("item", itemService.getItemDetails(id));
+        return "items/details";
+    }
+
     // TEMP sanity-check
     @GetMapping("/json")
     @ResponseBody
-    public List<Item> listItemsJson() {
-        return itemService.getAllItems();
+    public Item itemDetailsJson() {
+        return itemService.getItemDetails(1);
     }
 }
