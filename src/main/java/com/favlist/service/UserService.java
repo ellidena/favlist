24ad2452cb35adf4f9d750c6c1ca8bb8346bb4cf
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-03 18:44:29 +0200

    Add createUser()

--- src/main/java/com/favlist/service/UserService.java
+++ src/main/java/com/favlist/service/UserService.java
@@ -1,5 +1,6 @@
 package com.favlist.service;
 
+import com.favlist.model.User;
 import com.favlist.repository.UserRepository;
 import com.favlist.repository.WishlistRepository;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,11 @@ public class UserService {
     ) {
         this.userRepository = userRepository;
         this.wishlistRepository = wishlistRepository;
+    }
+
+    // create a user and their wishlist
+    public void createUser(User user) {
+        userRepository.insert(user);
+        wishlistRepository.insert(user.getUserId());
     }
 }
