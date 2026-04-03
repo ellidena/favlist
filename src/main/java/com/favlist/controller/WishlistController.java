f34730a53a8c0f3d00d3439f08fa04a2ba49faf9
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-03 20:55:36 +0200

    Add viewWishlist and json test endpoint

--- src/main/java/com/favlist/controller/WishlistController.java
+++ src/main/java/com/favlist/controller/WishlistController.java
@@ -1,9 +1,17 @@
 package com.favlist.controller;
 
+import com.favlist.model.Item;
+import com.favlist.model.Wishlist;
+import com.favlist.model.WishlistEntry;
 import com.favlist.service.UserService;
 import com.favlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/wishlist")
@@ -20,5 +28,19 @@ public class WishlistController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public String viewWishlist(Model model) {
+        int userId = 1; // temporary until login exists
+        Wishlist wishlist = userService.getWishlistForUser(userId);
 
+        model.addAttribute("entries", wishlistService.getEntries(wishlist.getWishlistId()));
+        return "wishlist/view";
+    }
+
+    @ResponseBody
+    @GetMapping("/json")
+    public List<WishlistEntry> showWishListJson(){
+        Wishlist wishlist = userService.getWishlistForUser(1);
+        return wishlistService.getEntries(wishlist.getWishlistId());
+    }
 }
