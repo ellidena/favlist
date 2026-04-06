3ca1eb9c873974b35aa83988ee73d243bab30cb2
ellidena <85340254+ellidena@users.noreply.github.com>
2026-04-06 19:03:05 +0200

    Add login postmapping to AuthController

--- src/main/java/com/favlist/controller/AuthController.java
+++ src/main/java/com/favlist/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.favlist.controller;
 
+import com.favlist.model.User;
 import com.favlist.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
@@ -16,5 +21,24 @@ public class AuthController {
     @GetMapping("/login")
     public String showLogin() {
         return "auth/login";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @RequestParam String username,
+            @RequestParam String password,
+            HttpSession session,
+            Model model
+    ) {
+        User user = userService.getUserByUsername(username);
+
+        if (user != null && user.getPassword().equals(password)) {
+            session.setAttribute("userId", user.getUserId());
+            return "redirect:/items";
+        }
+
+        model.addAttribute("error", true);
+        return "auth/login";
+
     }
 }
